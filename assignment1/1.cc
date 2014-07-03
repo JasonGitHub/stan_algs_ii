9935cd47a712db1a2fc060f6bbb3cc88a7152bec
jason <jasonwu2011@gmail.com>
2014-07-04 00:01:05 +0800

    init

---
+++ assignment1/1.cc
@@ -0,0 +1,38 @@
+/*
+ * main.cc
+ *
+ *  Created on: Jul 3, 2014
+ *      Author: Jason
+ */
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+bool diff(const pair<int, int> &a, const pair<int, int> &b) {
+  if (a.first - a.second != b.first - b.second) {
+    return a.first - a.second > b.first - b.second;
+  } else {
+    return a.first > b.first;
+  }
+}
+
+int main() {
+  int n, weight, length;
+  cin >> n;
+  vector<pair<int, int>> order;
+  for (int i = 0; i < n; ++i) {
+    cin >> weight >> length;
+    order.push_back(make_pair(weight, length));
+  }
+  sort(order.begin(), order.end(), diff);
+  long long time = 0, sum = 0;
+  for_each(order.begin(), order.end(), [&](pair<int,int> val){
+    time += val.second;
+    sum += time * val.first;
+  });
+  cout << sum;
+  return 0;
+}
---
+++ assignment1/2.cc
@@ -0,0 +1,34 @@
+/*
+ * main.cc
+ *
+ *  Created on: Jul 3, 2014
+ *      Author: Jason
+ */
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+bool divde(const pair<int, int> &a, const pair<int, int> &b) {
+  return static_cast<double>(a.first) / a.second > static_cast<double>(b.first) / b.second;
+}
+
+int main() {
+  int n, weight, length;
+  cin >> n;
+  vector<pair<int, int>> order;
+  for (int i = 0; i < n; ++i) {
+    cin >> weight >> length;
+    order.push_back(make_pair(weight, length));
+  }
+  sort(order.begin(), order.end(), divde);
+  long long time = 0, sum = 0;
+  for_each(order.begin(), order.end(), [&](pair<int,int> val){
+    time += val.second;
+    sum += time * val.first;
+  });
+  cout << sum;
+  return 0;
+}
