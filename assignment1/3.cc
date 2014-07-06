f477f764e83151e30c01fbabc336e3dd9e6f155d
jason <jasonwu2011@gmail.com>
2014-07-06 15:39:27 +0800

    update

--- assignment1/3.cc
+++ assignment1/3.cc
@@ -9,9 +9,11 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <deque>
 #include <utility>
 #include <cassert>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
@@ -44,19 +46,54 @@ int prim(const map<int, vector<pair<int, int> > > &graph) {
   while (!v_x.empty()) {
     int min_edge_cost = INT_MAX, min_node = -1;
     for (auto i = x.begin(); i != x.end(); ++i) {
-      for (auto j = v_x.begin(); j != v_x.end(); ++j) {
-        if (exist_edge(*i, *j, graph)) {
-          int cost = get_edge_cost(*i, *j, graph);
-          if (cost < min_edge_cost) {
-            min_edge_cost = cost;
-            min_node = *j;
-          }
+      for (auto j = graph.at(*i).begin(); j != graph.at(*i).end(); ++j) {
+        if (v_x.find(j->first) != v_x.end() && j->second < min_edge_cost) {
+          min_edge_cost = j->second;
+          min_node = j->first;
         }
       }
     }
     x.insert(min_node);
     v_x.erase(min_node);
     sum += min_edge_cost;
+  }
+  return sum;
+}
+
+struct Node {
+  int id;
+  int cost;
+  Node(int id, int cost) : id(id), cost(cost) {}
+  bool operator>(Node that) const {
+    return cost > that.cost;
+  }
+};
+
+int prim_heap(const map<int, vector<pair<int, int> > > &graph) {
+  set<int> x;
+  deque<Node> v_x;
+  int start = -1;
+  for (auto i = graph.begin(); i != graph.end(); ++i) {
+    if (i == graph.begin()) {
+      start = i->first;
+      x.insert(start);
+    } else {
+      Node tmp(i->first, INT_MAX);
+      if (exist_edge(tmp.id, start, graph)) tmp.cost = get_edge_cost(tmp.id, start, graph);
+      v_x.push_back(tmp);
+    }
+  }
+  make_heap(v_x.begin(), v_x.end(), greater<Node>());
+  int sum = 0;
+  while (!v_x.empty()) {
+    Node min_node = v_x.front();
+    x.insert(min_node.id);
+    v_x.pop_front();
+    sum += min_node.cost;
+    for (auto i = v_x.begin(); i != v_x.end(); ++i) {
+      if (exist_edge(min_node.id, i->id, graph)) i->cost = min(get_edge_cost(min_node.id, i->id, graph), i->cost);
+    }
+    make_heap(v_x.begin(), v_x.end(), greater<Node>());
   }
   return sum;
 }
