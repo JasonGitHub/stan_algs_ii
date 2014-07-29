07fefd20d3b2489ac6056437eb8fe606bde3df4b
jason <jasonwu2011@gmail.com>
2014-07-30 01:01:00 +0800

    update

--- assignment3/2.cc
+++ assignment3/2.cc
@@ -4,16 +4,15 @@
 using namespace std;
 
 int knapsack(const vector<int> &v, const vector<int> &w, int W, int n) {
-  vector<int> prev(W + 1, 0), curr(W + 1, 0);
+  vector<int> V(W + 1, 0);
   for (int i = 1; i <= n; ++i) {
-    for (int x = 0; x <= W; ++x) {
-      int case_1 = x < w[i] ? INT_MIN : prev[x - w[i]] + v[i];
-      int case_2 = prev[x];
-      curr[x] = max(case_1, case_2);
+    for (int x = W; x >= 0; --x) {
+      int case_1 = x < w[i] ? INT_MIN : V[x - w[i]] + v[i];
+      int case_2 = V[x];
+      V[x] = max(case_1, case_2);
     }
-    swap(prev, curr);
   } 
-  return prev[W];
+  return V[W];
 }
 
 int main() {
